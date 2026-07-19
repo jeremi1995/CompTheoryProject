b338fcc61310fce5535e0626b5d70c8e3e850500
Jeremy Duong <jeremibq@gmail.com>
2026-07-18 17:55:18 -0700

    chore: updated variable names

--- test_cases/test_case.py
+++ test_cases/test_case.py
@@ -2,9 +2,9 @@ from automaton import AutomatonResult
 
 class TestCase:
     caseId: str
-    inputString: str
-    expectedResult: AutomatonResult
-    def __init__(self, caseId: str, inputString: str, expectedResult: AutomatonResult):
+    input_string: str
+    expected_result: AutomatonResult
+    def __init__(self, caseId: str, input_string: str, expected_result: AutomatonResult):
         self.caseId = caseId
-        self.inputString = inputString
-        self.expectedResult = expectedResult
+        self.input_string = input_string
+        self.expected_result = expected_result
